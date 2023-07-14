0a8d573be2720e92fa3276a2f28fa0f917749c46
muazaiman45 <87751717+muazmn@users.noreply.github.com>
2023-07-14 08:57:48 +0800

    fix err

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -11,15 +11,15 @@ class AuthService implements AuthProvider {
       provider.createUser(email: email, password: password);
 
   @override
-  // TODO: implement currentUser
   AuthUser? get currentUser => provider.currentUser;
 
   @override
-  Future<AuthUser> logIn({required String email, required String password}) => provider.logIn(email: email, password: password)
+  Future<AuthUser> logIn({required String email, required String password}) =>
+      provider.logIn(email: email, password: password);
 
   @override
   Future<void> logOut() => provider.logOut();
 
   @override
-  Future<void> sendEmailVerification() =>provider.sendEmailVerification();
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
 }
