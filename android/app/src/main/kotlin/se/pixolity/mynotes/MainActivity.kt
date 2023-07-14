dc083f95d75b7334bb35c26e06839b50d26e70db
muazaiman45 <87751717+muazmn@users.noreply.github.com>
2023-07-14 22:26:00 +0800

    restore

---
+++ android/app/src/main/kotlin/se/pixolity/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package se.pixolity.flutter_docbook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_docbook/constants/routes.dart';
+import 'package:flutter_docbook/services/auth/auth_service.dart';
+import 'package:flutter_docbook/views/login_view.dart';
+import 'package:flutter_docbook/views/notes_view.dart';
+import 'package:flutter_docbook/views/register_view.dart';
+import 'package:flutter_docbook/views/verify_email_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+      routes: {
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
+      },
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: AuthService.firebase().initialize(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = AuthService.firebase().currentUser;
+            if (user != null) {
+              if (user.isEmailVerified) {
+                return const NotesView();
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
